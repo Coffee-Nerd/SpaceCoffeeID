b8df2f22b04773a024f8504e629d3eb2baecf0d2
AsmodeusBrooding <asmodeusbrooding@gmail.com>
2023-10-07 22:32:00 +1100

    Navbar rework

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,10 +1,20 @@
-var navbar = document.querySelector('.navbar');
-const navbarCollapse = document.getElementById('navbar-collapse');
-window.addEventListener('scroll', function() {
-    if (window.scrollY >= 1) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-        navbarCollapse.classList.remove('show');
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.getElementById("menu-btn");
+  
+    function toggleNavbarBlur() {
+      if (window.scrollY > 1 || menuBtn.checked) {
+        navbar.classList.add("scrolled");
+      } else {
+        navbar.classList.remove("scrolled");
+      }
     }
-});
+  
+    window.addEventListener("scroll", toggleNavbarBlur);
+    
+    menuBtn.addEventListener("change", toggleNavbarBlur);
+  
+    // Run it once to set the initial state
+    toggleNavbarBlur();
+  });
+  
